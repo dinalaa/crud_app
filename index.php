163c32286ce50b8f4535e88408c87869d51b68d0
Dina Alaa <alaad2456@gmail.com>
2020-03-10 00:17:36 +0200

    second commit

--- index.php
+++ index.php
@@ -3,8 +3,8 @@ include 'connect.php';
 if(isset($_POST['submit'])){
     $name = $_POST['name'];
     $password = $_POST['password'];
-    $sql = "INSERT INTO `items` (`name`, `password`) VALUES ('$name', '$password')";
-    $res = $conn -> query($sql);
+    $query = "INSERT INTO `items` (`name`, `password`) VALUES ('$name', '$password')";
+    $res = $conn -> exec($query);
     if($res){
         echo "yes";
     } else {
